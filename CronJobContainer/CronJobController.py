7e7009cbab02131e2db7a5536996951078528317
TaiShan  Lin <taishanlin@TaiShans-MacBook-Pro.local>
2022-12-29 17:49:46 +0800

    reworked cronjob

--- CronJobContainer/CronJobController.py
+++ CronJobContainer/CronJobController.py
@@ -3,10 +3,6 @@ from GetOptionsChain.GetHistoricalReturns import updateAllUnderlyingSecuritiesIn
 from GetOptionsChain.RemoveOldOptions import removeOldOptions
 from GetOptionsChain.Model import updateAllModelCalculatedOptions
 
-from   multiprocessing import freeze_support
-import pycron 
-import datetime 
-
 cronJobContainer = {
     'Options Cron': {
         'startMessage': 'Starting Options Cron',
@@ -17,38 +13,14 @@ cronJobContainer = {
         'startMessage': 'Starting Historical Returns Cron',
         'function': updateAllUnderlyingSecuritiesInfo,
     },
+    'Options Calculator': {
+        'startMessage': 'Starting Options Calculator Cron',
+        'function': updateAllModelCalculatedOptions,
+        'freeze_support': True,
+    },
     'Remove Old Data': {
         'startMessage': 'Starting Remove Old Data Cron',
         'function': removeOldOptions 
     },
-    'Options Calculator': {
-        'startMessage': 'Starting Options Calculator Cron',
-        'function': updateAllModelCalculatedOptions
-    }
 }
-
-def cronJobInit():
-    if pycron.is_now('*/10 * * * *') == True:
-        print(cronJobContainer['Options Cron']['startMessage']);
-        freeze_support();
-        cronJobContainer['Options Cron']['function']()
-        print('Cron Job Options Complete for time: ', datetime.datetime.now(), '');
-
-    # if pycron.is_now('*/11 * * * *') == True:
-    #     print(cronJobContainer['Remove Old Data']['startMessage']);
-    #     cronJobContainer['Remove Old Data']['function']()
-    #     print('Cron Job Remove Old Data Complete for time: ', datetime.datetime.now(), '');
-
-    elif pycron.is_now('*/15 * * * *') == True:
-        print(cronJobContainer['Historical Returns']['startMessage']);
-        cronJobContainer['Historical Returns']['function']()
-        print('Cron Job Historical Returns Complete for time: ', datetime.datetime.now(), '');
-
-    elif pycron.is_now('*/25 * * * *') == True:
-        print(cronJobContainer['Options Calculator']['startMessage']);
-        freeze_support();
-        cronJobContainer['Options Calculator']['function']()
-        print('Cron Job Options Calculator Complete for time: ', datetime.datetime.now(), '');
-
-    else: return None;
 
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from CronJobContainer.CronJobController import cronJobInit
+from CronJobContainer.CronJobController import cronJobContainer
+from multiprocessing import freeze_support
 import time 
 # This is the main entry point for the application and will run the cron job every 60 seconds.
 # On Windows the subprocesses will import (i.e. execute) the main module at start. You need to insert an if __name__ == '__main__': guard in the main module to avoid creating subprocesses recursively.
 if __name__ == '__main__':
-    while (1):
-        cronJobInit();
-        time.sleep(30)
+    while True:
+        for cronJob in cronJobContainer:
+            if cronJobContainer[cronJob]['freeze_support'] == True:
+                freeze_support()
+            cronJobContainer[cronJob]['function']()
+        time.sleep(120000)
