f020a16a2dcaeafe80e7db6750b2fca3d3a6845e
TaiShan  Lin <taishanlin@TaiShans-MacBook-Pro.local>
2022-12-29 18:31:24 +0800

    latest from development

--- CronJobContainer/CronJobController.py
+++ CronJobContainer/CronJobController.py
@@ -2,6 +2,10 @@ from GetOptionsChain.GetOptions import updateAllOptions
 from GetOptionsChain.GetHistoricalReturns import updateAllUnderlyingSecuritiesInfo
 from GetOptionsChain.Model import updateAllModelCalculatedOptions
 
+import pycron
+import datetime 
+from multiprocessing import freeze_support
+
 cronJobContainer = {
     'Options Cron': {
         'startMessage': 'Starting Options Cron',
@@ -19,4 +23,19 @@ cronJobContainer = {
         'freeze_support': True,
     },
 }
+
+def cronJobInit():
+    if pycron.is_now('*/10 * * * *') == True:
+        print(cronJobContainer['Options Cron']['startMessage']);
+        freeze_support();
+        cronJobContainer['Options Cron']['function']();
+        cronJobContainer['Options Calculator']['function']();
+        print('Cron Job Options Complete for time: ', datetime.datetime.now(), '');
+
+    elif pycron.is_now('*/15 * * * *') == True:
+        print(cronJobContainer['Historical Returns']['startMessage']);
+        cronJobContainer['Historical Returns']['function']()
+        print('Cron Job Historical Returns Complete for time: ', datetime.datetime.now(), '');
+
+    else: return None;
 
--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-from CronJobContainer.CronJobController import cronJobContainer
-from multiprocessing import freeze_support
+from   CronJobContainer.CronJobController import cronJobInit
+from   multiprocessing import freeze_support
 import time 
 # This is the main entry point for the application and will run the cron job every 60 seconds.
 # On Windows the subprocesses will import (i.e. execute) the main module at start. You need to insert an if __name__ == '__main__': guard in the main module to avoid creating subprocesses recursively.
 if __name__ == '__main__':
-    loopNumber = 0;
     while True:
-        for cronJob in cronJobContainer:
-            if cronJobContainer[cronJob]['freeze_support'] == True:
-                freeze_support()
-            cronJobContainer[cronJob]['function']();
-        loopNumber += 1;
-
-        print('Completed...', loopNumber, 'loops')
-        time.sleep(120000);
+        cronJobInit();
+        time.sleep(60);
