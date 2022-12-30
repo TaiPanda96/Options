202ecfca7e38083771b747c72d2789050bb2ebfc
TaiShan  Lin <taishanlin@TaiShans-MacBook-Pro.local>
2022-12-30 11:02:40 +0800

    latest

--- CalculatorCron.py
+++ CalculatorCron.py
@@ -1,9 +1,8 @@
-from   CronJobContainer.CronJobController import cronJobInit,optionsCalculatorCronJob
-from   multiprocessing import freeze_support
+from   CronJobContainer.CronJobController import optionsCalculatorCronJob
 import time 
 # This is the main entry point for the application and will run the cron job every 60 seconds.
 # On Windows the subprocesses will import (i.e. execute) the main module at start. You need to insert an if __name__ == '__main__': guard in the main module to avoid creating subprocesses recursively.
 if __name__ == '__main__':
     while True:
-        cronJobInit();
+        optionsCalculatorCronJob();
         time.sleep(30);
