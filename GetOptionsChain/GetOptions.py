a84a335b74cc7d809b26f53d97b86c380cca635c
TaiShan  Lin <taishanlin@TaiShans-MacBook-Pro.local>
2023-02-03 19:33:25 -0500

    fixed API call

--- GetOptionsChain/GetOptions.py
+++ GetOptionsChain/GetOptions.py
@@ -2,9 +2,22 @@
 import requests
 import traceback
 import pytz
-from   datetime import datetime
-from   multiprocessing import Pool
-from   Postgres.InsertQuery import insertQuery
+from datetime import datetime
+from multiprocessing import Pool
+from Postgres.InsertQuery import insertQuery
+
+debug = False;
+
+headers = {
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
+    'authority': 'www.cnbc.com',
+    'pragma': 'no-cache',
+    'cache-control': 'no-cache',
+    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
+    'accept': 'application/json, text/plain, */*',
+    'sec-ch-ua-mobile': '?0',
+}
+
 
 columns = [
     'type',
@@ -26,22 +39,67 @@ columns = [
     'inTheMoney'
 ];
 
+
+def getOptionsChainByExpiration(symbol=None, expirationDate=None):
+    if (symbol is None): symbol = 'AAPL';
+    if (expirationDate is None): return None;
+    try:
+        url = 'https://query1.finance.yahoo.com/v7/finance/options/' + \
+            symbol + '?date=' + str(expirationDate);
+        response = requests.get(url, headers=headers).json();
+        if response:
+            if response.get('optionChain') is None: return
+            if response.get('optionChain').get('result') is None: return
+            optionsInExpirationDate = response['optionChain']['result'][0] if len(
+                response['optionChain']['result']) > 0 else []
+            if len(optionsInExpirationDate) == 0: return
+            useOptionExpirations = optionsInExpirationDate['options'][0] if len(
+                optionsInExpirationDate['options']) > 0 else []
+            if len(useOptionExpirations) == 0: return
+            calls = useOptionExpirations['calls'];
+            puts = useOptionExpirations['puts'];
+            for call in calls: {
+                **call, **{'type': 'call', 'expiration': expirationDate}};
+            for put in puts: {
+                **put, **{'type': 'put', 'expiration': expirationDate}};
+
+        return {
+            expirationDate: {
+                'calls': calls,
+                'puts': puts
+            }
+        };
+    except Exception as error:
+        print(error)
+        traceback.print_exc()
+
+
 def getOptionsChain(symbol=None):
     if symbol is None: symbol = 'AAPL';
     try:
-        url = 'https://kf1wexbj85.execute-api.us-east-2.amazonaws.com/Prod/get-options?ticker={}'.format(
+        url = 'https://query1.finance.yahoo.com/v7/finance/options/{}'.format(
             symbol)
-        response = requests.get(url)
+        response = requests.get(url, headers=headers);
         if response.status_code == 200:
-            return response.json()
+            options = response.json();
+            if options is None: return
+            expirationDates = options['optionChain']['result'];
+            useExpirationDates = expirationDates[0]['expirationDates'] if expirationDates and len(
+                expirationDates) > 0 else []
+            optionsData = (getOptionsChainByExpiration(symbol, expirationDate)
+                           for expirationDate in useExpirationDates);
+            return {
+                'symbol': symbol,
+                'optionsData': list(optionsData)
+            }
         else:
             return None
     except Exception as e:
         print(e)
-        traceback.print_exc(exec_info=True)
+        traceback.print_exc()
 
 
-def standardizeOptionsData(optionsChain, type='call', symbol = ''):
+def standardizeOptionsData(optionsChain, type='call', symbol=''):
     optionsData = {
         "type": type,
         "symbol": symbol,
@@ -70,14 +128,17 @@ def standardizeOptionsData(optionsChain, type='call', symbol = ''):
 
     return optionsData
 
-def updateOptionsData(symbol = None):
+
+def updateOptionsData(symbol=None):
     if symbol is None: symbol = 'AAPL'
     updateCalls = []
-    updatePuts  = []
+    updatePuts = []
     try:
         optionsChain = getOptionsChain(symbol)
         if optionsChain is None: return None
-        options = optionsChain.get('optionsData', None) if optionsChain is not None else None
+        options = optionsChain.get(
+            'optionsData', None) if optionsChain is not None else None
+        if options == []: return
         dates = [i.keys() for i in options]
         # Flatten the list of lists
         dates = [item for sublist in dates for item in sublist]
@@ -100,19 +161,22 @@ def updateOptionsData(symbol = None):
 
             updateCalls.append([standardizeOptionsData(i, 'call', symbol)
                                for i in calls])
-            updatePuts.append([standardizeOptionsData(i, 'put',symbol) 
+            updatePuts.append([standardizeOptionsData(i, 'put', symbol)
                                for i in puts])
 
         # Flatten the list of lists
         updateCalls = [tuple(item.values())
                        for sublist in updateCalls for item in sublist]
-        updatePuts  = [tuple(item.values())
+        updatePuts = [tuple(item.values())
                       for sublist in updatePuts for item in sublist]
 
         # Insert Calls
-        insertQuery('options',columns,updateCalls);
+        if debug: print('ROWS INSERTED: {} for {} calls'.format(
+            len(updateCalls), symbol))
+        insertQuery('options', columns, updateCalls);
 
         # Insert Puts
+        if debug: print('ROWS INSERTED: {} for {} puts'.format(len(updatePuts), symbol))
         insertQuery('options',columns,updatePuts);
 
     except Exception as e:
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from GetOptionsChain.Model import updateAllModelCalculatedOptions
-from GetOptionsChain.GetOptions import updateAllOptions
+from GetOptionsChain.GetOptions import updateAllOptions,getOptionsChain
 
 # This is the main entry point for the application and will run the cron job every 60 seconds.
 # On Windows the subprocesses will import (i.e. execute) the main module at start. You need to insert an if __name__ == '__main__': guard in the main module to avoid creating subprocesses recursively.
 if __name__ == '__main__':
+   # data = getOptionsChain('AAPL');
+   # print(data);
    updateAllOptions();
-   updateAllModelCalculatedOptions();
+   # updateAllModelCalculatedOptions();
